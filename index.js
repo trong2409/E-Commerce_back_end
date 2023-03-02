63af7b8ce4c17fdcf80ae96a2b516f0af7454cae
trong2409 <nvtrong249@gmail.com>
2023-03-02 11:59:53 +0700

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const cors = require("cors");
 
 dbConnect();
 app.use(morgan("dev"));
-app.use(cors({ credentials: true }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
